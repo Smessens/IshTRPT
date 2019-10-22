de6cca67516567aa52d93492e6c1ba54291b30aa
glegat <guillaume.legat@student.uclouvain.be>
2019-10-22 16:26:17 +0200

    adding the basic test

--- tests/tests.c
+++ tests/tests.c
@@ -5,6 +5,16 @@
 #include <sys/types.h>
 #include "CUnit/Basic.h"
 #include "CUnit/CUnit.h"
+#include "CUnit/Console.h"
+
+void test_basic(void)
+{
+  printf("FIRST TEST : test basique\n");
+  system("../src/receiver -f test_basique_out.dat :: 2456");
+  system("../src/senderprof localhost 1348 < test_basique_in.dat");
+  int diff = system("diff test_basique_in.dat test_basique_out.dat");
+  CU_ASSERT_EQUAL(WEXITSTATUS(diff),0);
+}
 
 int main(int argc, char const *argv[]) {
   CU_pSuite pSuite = NULL;
@@ -12,10 +22,19 @@ int main(int argc, char const *argv[]) {
   if(CUE_SUCCESS != CU_initialize_registry()) {
      return CU_get_error();
    }
-   pSuite = CU_add_suite("Suite de tests sur le protocole", NULL, NULL);
+   pSuite = CU_add_suite("Ma suite de tests", NULL, NULL);
    if(NULL == pSuite) {
       CU_cleanup_registry();
       return CU_get_error();
     }
-  return 0;
+    if ((NULL == CU_add_test(pSuite, "Test basique", test_basic)))
+{
+  CU_cleanup_registry();
+  return CU_get_error();
+}
+CU_basic_set_mode(CU_BRM_SILENT);
+CU_basic_run_tests();
+CU_basic_show_failures(CU_get_failure_list());
+CU_cleanup_registry();
+return CU_get_error();
 }
